351a02b0972c7a1e8e9b2f277190aaf61d4b240a
hycru <luedjis4047v@gmail.com>
2023-05-19 22:49:31 -0300

    2

---
+++ a.js
@@ -0,0 +1,15 @@
+function sim()
+{
+    let a = 1
+    let b = 2
+    let c = a + b
+    document.getElementById('num').innerHTML = c +' sim'
+    alert(c)
+}
+function sim2()
+{
+    let a = prompt()
+    let b = getElementById('bt')
+    document.getElementById('rslt').innerHTML = a && b
+
+}
